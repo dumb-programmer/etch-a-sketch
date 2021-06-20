f6a0c1eb7e1aa0106bb581191d7e4bdb7e0adf5e
Asad Khan <asadkhan6192@gmail.com>
2021-06-20 22:56:39 +0500

    Add hover effect

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ for (let i = 0; i < 17; i++) {
     div.setAttribute('id', 'child')
     container.appendChild(div);
 }
+
+let divs = document.querySelectorAll("#child")
+divs = Array.from(divs)
+console.log(divs)
+divs.forEach(element => {
+    element.addEventListener('mouseleave', () => {
+        element.style.backgroundColor = "red";
+    })
+});
