0dab068853b1a9bb73bc07c5390bd0da8450e465
Asad Khan <asadkhan6192@gmail.com>
2021-06-25 11:46:43 +0500

    Center the grid and button

--- script.js
+++ script.js
@@ -3,15 +3,12 @@ const container = document.querySelector(".container")
 function makeGrid(squares) {
     container.style.gridTemplateColumns = `repeat(${squares},1fr)`;
     container.style.gridTemplateRows = `repeat(${squares},1fr)`;
-    console.log(container.style.gridTemplateColumns)
-    console.log(container.style.gridTemplateRows)
     for (let i = 1; i <= (squares * squares); i++) {
         const div = document.createElement('div');
         div.setAttribute('id', 'child')
         container.appendChild(div);
     }
-
-    console.log(document.querySelectorAll("#child").length)
+    attachEvent()
 }
 
 function clearGrid() {
@@ -22,14 +19,13 @@ function clearGrid() {
     })
 }
 
+
+// Make a 16x16 grid when the page loads
 makeGrid(16);
 
 function attachEvent() {
-
-
     let divs = document.querySelectorAll("#child")
     divs = Array.from(divs)
-
 
     divs.forEach(element => {
         element.addEventListener('mouseleave', () => {
@@ -38,19 +34,13 @@ function attachEvent() {
     });
 }
 
-attachEvent()
-
-
 const btn = document.querySelector("#reset");
 
 btn.addEventListener('click', () => {
     clearGrid();
-    const squares = +prompt("How many squares?");
-    if(0 < squares && squares < 101){
-        makeGrid(squares)
-        attachEvent()
+    let squares = +prompt("How many squares per side to make the new grid(0-100)?");
+    while (squares <= 0 || squares > 100) {
+        squares = +prompt("How many squares per side to make the new grid(0-100)?");
     }
-    else{
-        container.textContent = "Can't compute such a large grid"
-    }
+    makeGrid(squares)
 })
